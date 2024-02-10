b69500b627620cf76e654c8c285feaff73ed1a39
AntonSmart11 <antoniooc11@outlook.com>
2024-02-10 12:27:14 -0600

    Acutalización a la versión 8.2.2 y cambio de sdk a 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.antonsmart.iceenberg"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.antonsmart.iceenberg"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
