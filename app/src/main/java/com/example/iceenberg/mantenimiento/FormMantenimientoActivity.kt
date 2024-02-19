d5a1389ad30f8124f70868280a71b63a5d8f8624
Miguel5018 <miguelgerardosalvadorperez@gmail.com>
2024-02-18 19:12:29 -0600

    Updates de mantenimientos, recarga de recycler view al insertar

--- app/src/main/java/com/example/iceenberg/mantenimiento/FormMantenimientoActivity.kt
+++ app/src/main/java/com/example/iceenberg/mantenimiento/FormMantenimientoActivity.kt
@@ -3,9 +3,12 @@ package com.example.iceenberg.mantenimiento
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.widget.Toast
 import com.example.iceenberg.Controllers.MaintenanceController
 import com.example.iceenberg.Database.DatabaseHelper
+import com.example.iceenberg.Objects.Location
+import com.example.iceenberg.Objects.Maintenance
 import com.example.iceenberg.R
 import com.example.iceenberg.databinding.ActivityFormMantenimientoBinding
 
@@ -22,8 +25,32 @@ class FormMantenimientoActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbarFormMantenimientos)
 
+
+        //Se confirma si llega un dato a la activity
+        val intent = intent
+        var titleFormMaintenance = getString(R.string.main_form_maintenance_title)
+        var nameEditText = ""
+        var costEditText = 0
+        var maintenanceID = 0
+
+        if (intent != null && intent.hasExtra("maintenance")) {
+            Toast.makeText(this, "Llego", Toast.LENGTH_SHORT).show()
+            val maintenance = intent.getSerializableExtra("maintenance") as? Maintenance
+            titleFormMaintenance = getString(R.string.main_form_maintenance_title_edit)
+            val maintenanceId = maintenance?.id!!
+            maintenanceID = maintenanceId
+            nameEditText = maintenance.name
+            costEditText = maintenance.cost.toInt()
+
+            binding.buttonsEdit.visibility = View.VISIBLE
+        } else {
+            Toast.makeText(this, "No llego", Toast.LENGTH_SHORT).show()
+            binding.buttonsAdd.visibility = View.VISIBLE
+        }
+
+
         supportActionBar?.apply {
-            title = getString(R.string.main_form_location_title)
+            title = getString(R.string.main_form_maintenance_title)
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
@@ -42,8 +69,13 @@ class FormMantenimientoActivity : AppCompatActivity() {
         var costMaintenance: String;
         var costMaintenanceNum: Double;
 
+
+        //Setear editText con el texto por defecto
+        binding.nombreMantenimiento.setText(nameEditText)
+        binding.costoMantenimiento.setText(costEditText.toString())
+
         //Tomar los datos del usuario
-        binding.btnEdit.setOnClickListener {
+        binding.btnaAdd.setOnClickListener {
             nameMaintenance = binding.nombreMantenimiento.text.toString()
             costMaintenance = binding.costoMantenimiento.text.toString()
 
@@ -68,7 +100,35 @@ class FormMantenimientoActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, getString(R.string.empty), Toast.LENGTH_SHORT).show()
             }
+        }
 
+        //Editar datos en la BD
+        binding.btnEdit.setOnClickListener {
+
+            nameMaintenance = binding.nombreMantenimiento.text.toString()
+            costMaintenance = binding.costoMantenimiento.text.toString()
+
+            if (nameMaintenance.isNotEmpty() && costMaintenance.isNotEmpty()) {
+                costMaintenanceNum = costMaintenance.toDouble()
+
+                //Insertar datos a la BD
+                val exitoso = maintenanceController.updateMaintenance(maintenanceID,nameMaintenance,costMaintenanceNum);
+
+                if (exitoso) {
+                    Toast.makeText(this, getString(R.string.successInsert), Toast.LENGTH_SHORT).show()
+
+                    val handler = Handler()
+                    val runnable = Runnable {
+                        onBackPressed()
+                    }
+
+                    handler.postDelayed(runnable, 150) // 3 segundos de retraso
+                } else {
+                    Toast.makeText(this, getString(R.string.errorInsert), Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.empty), Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
--- app/src/main/java/com/example/iceenberg/mantenimiento/MantenimientoActivity.kt
+++ app/src/main/java/com/example/iceenberg/mantenimiento/MantenimientoActivity.kt
@@ -3,12 +3,20 @@ package com.example.iceenberg.mantenimiento
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.iceenberg.Adapters.MaintenanceAdapter
+import com.example.iceenberg.Controllers.MaintenanceController
+import com.example.iceenberg.Database.DatabaseHelper
+import com.example.iceenberg.Objects.Maintenance
 import com.example.iceenberg.R
 import com.example.iceenberg.databinding.ActivityMantenimientoBinding
 
-class MantenimientoActivity : AppCompatActivity() {
+class MantenimientoActivity : AppCompatActivity(), MaintenanceAdapter.OnItemClickListener {
 
     private lateinit var binding: ActivityMantenimientoBinding
+    private lateinit var dbHelper: DatabaseHelper
+    private lateinit var maintenanceController: MaintenanceController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +40,32 @@ class MantenimientoActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //conexión con a la bd y llamado del controlador
+        dbHelper = DatabaseHelper(this)
+        maintenanceController = MaintenanceController(dbHelper)
 
+        //ReciclerView
+        val maintenances = maintenanceController.getMaintenance()
+
+        val recyclerView: RecyclerView = findViewById(R.id.mantenimientos_recyclerView)
+        val adapter = MaintenanceAdapter(maintenances,this)
+        recyclerView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Actualizar la lista de mantenimientos en el RecyclerView
+        val maintenances = maintenanceController.getMaintenance()
+        (binding.mantenimientosRecyclerView.adapter as? MaintenanceAdapter)?.updateList(maintenances)
+    }
+
+    override fun onItemClick(maintenance: Maintenance){
+        val intent = Intent(this, FormMantenimientoActivity::class.java)
+        intent.putExtra("maintenance",maintenance)
+        startActivity(intent)
     }
 }
