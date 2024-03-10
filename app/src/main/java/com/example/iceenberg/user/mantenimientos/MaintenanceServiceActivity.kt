c190a5ab1c90f85821942f8a57fd9f311b7a0c25
Miguel5018 <miguelgerardosalvadorperez@gmail.com>
2024-03-10 15:38:18 -0600

    update de branch

--- app/src/main/java/com/example/iceenberg/user/mantenimientos/MaintenanceServiceActivity.kt
+++ app/src/main/java/com/example/iceenberg/user/mantenimientos/MaintenanceServiceActivity.kt
@@ -8,5 +8,8 @@ class MaintenanceServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maintenance_service)
+
+
+        //update de la branch
     }
 }
